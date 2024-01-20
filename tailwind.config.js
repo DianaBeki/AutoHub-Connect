4c17acd123a1cf6572a45514f4094f6f1675d973
Diana Beki <dianabeki54@gmail.com>
2024-01-20 04:17:10 +0300

    Add primary color and dark color for my components

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,24 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         signature: ['Rubik Burned'],
       },
+      colors: {
+        primary: "#fca311",
+        secondary: "#ffffff",
+        dark: "#000000"
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem"
+        }
+      }
     },
   },
   plugins: [],
