a2f1b079c2f545df30b2f3cf854e2467f6bdf930
Diana Beki <dianabeki54@gmail.com>
2024-01-21 19:09:53 +0300

    Add padding top to div container

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Services = () => {
   return (
     <div>
-      
+      <div className="container pt-[10rem]">
+        <span>
+        </span>
+      </div>
     </div>
   )
 }
