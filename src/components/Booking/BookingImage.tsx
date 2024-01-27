efe0537c15d125c04c746083f976cb8be335a921
Diana Beki <dianabeki54@gmail.com>
2024-01-27 04:44:35 +0300

    Remove padding bottom

--- src/components/Booking/BookingImage.tsx
+++ src/components/Booking/BookingImage.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {toast } from 'react-toastify';
 import alignedCars from '../../assets/booking/alignedcars.jpg';
 
-const BookingForm = () => {
+const BookingImage = () => {
   return (
-    <div className="bg-secondary w-full dark:bg-dark pb-[5rem]">
+    <div className="bg-secondary w-full dark:bg-dark">
       <div className="container pt-[6rem] md:pt-[8rem]">
-<div className="border-2 dark:border-dark">
+<div className="">
   <img src={alignedCars} alt="" className="object-cover mx-auto w-full h-56" />
 </div>
       </div>
@@ -14,4 +13,4 @@ const BookingForm = () => {
   )
 }
 
-export default BookingForm
+export default BookingImage
