674284be8b1aef6df1bb6a5f4e7f8fbcbf6ac0a0
Diana Beki <dianabeki54@gmail.com>
2024-01-30 02:13:34 +0300

    Import all components links

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import HomeImg from '../Home/Home'
+import OurServices from '../Services/OurServices'
+import ChooseUs from '../Services/ChooseUs'
+import CarsDetail from '../Cars/CarsDetail'
+import Testimonials from '../Cars/Testimonials'
+import AboutUs from '../About/AboutUs'
+import BookingImage from '../Booking/BookingImage'
+import BookingForm from '../Booking/BookingForm'
 
 const Home = () => {
   return (
     <div>
      <HomeImg />
+     <OurServices />
+     <ChooseUs />
+     <CarsDetail />
+     <Testimonials />
+     <AboutUs />
+     <BookingImage />
+     <BookingForm />
     </div>
   )
 }
