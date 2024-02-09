ec2309a60ba3b45b2a2c8afd80ecc24f6d6ca244
Diana Beki <dianabeki54@gmail.com>
2024-02-09 16:58:51 +0300

    Add padding and leading to h1 element

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { GiAutoRepair } from "react-icons/gi";
-import icon from '../../assets/home/icon.png';
 
 const HomeImg = () => {
   return (
@@ -11,7 +10,7 @@ const HomeImg = () => {
         </div>
         <div className="mt-10">
         <h1  
-        className="text-center text-2xl md:text-5xl tracking tight text-secondary">
+        className="text-center text-2xl md:text-5xl text-secondary leading-[45px] md:leading-[62px]">
             CAR RENTAL AND THE <br />
              CAR HUB WELCOMES <br />
               YOU
@@ -19,7 +18,7 @@ const HomeImg = () => {
         </div>
         <div 
         className="text-center mt-3">
-          <h3 className="bg-primary inline p-1 text-2xl">Life on Wheels</h3>
+          <h3 className="bg-primary inline p-1 text-2xl py-3 px-5">Life on Wheels</h3>
         </div>
       </div>
     </div>
